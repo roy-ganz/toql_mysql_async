8a5c7a49c83b624e21ad36640b56e66f6f7a9859
roy-ganz <roy.ganz@glanis.biz>
2021-07-20 16:44:45 +0200

    Add ToqlApi

--- src/lib.rs
+++ src/lib.rs
@@ -6,41 +6,22 @@
 
 
 
-use crate::row::Row;
-
-//use toql::mutate::collection_delta_sql;
-
-
-
-use toql::keyed::Keyed;
-use toql::page::Page;
-
-use toql::query::Query;
 
 use toql::sql_mapper_registry::SqlMapperRegistry;
 
 use toql::error::ToqlError;
- use toql::paths::Paths;
+ 
 
-use core::borrow::Borrow;
+
 use toql::alias_format::AliasFormat;
 
-use crate::error::ToqlMySqlAsyncError;
-use toql::prelude::FromRow;
-
-
-//use crate::row::FromResultRow;
 use std::{
-    borrow::BorrowMut,
+    
     collections::{HashMap, HashSet},
     sync::RwLockReadGuard,
 };
 
- use toql::fields::Fields;
- use toql::backend::update::{update, Update};
- use toql::backend::insert::{insert, Insert};
- use toql::backend::load::{load, Load};
- use toql::backend::count::{count, Count};
+ 
  
 
 
@@ -55,6 +36,8 @@ pub mod row;
 
 pub mod backend;
 
+pub mod toql_api;
+
 #[cfg(test)]
 mod test;
 
@@ -67,7 +50,7 @@ use crate::backend::MySqlAsyncBackend;
 
 use toql::{prelude::{Context, Cache, SqlArg}};
 
-use result::Result;
+
 
 
 pub struct MySqlAsync<'a, C>
@@ -129,97 +112,4 @@ impl<'a, C> MySqlAsync<'a, C> where
     pub fn set_aux_param(&mut self, name: String, value: SqlArg) {
         &self.backend.context.aux_params.insert(name, value);
     }
-
-    pub async fn insert_one<T>(&mut self, entity: &mut T, paths: Paths) -> Result<u64>
-    where
-        T: Insert 
-    {
-        self.insert_many::<T, _>(&mut [entity], paths).await
-    }
-
-    /// Insert one struct.
-    ///
-    /// Skip fields in struct that are auto generated with `#[toql(skip_inup)]`.
-    /// Returns the last generated id.
-    pub async fn insert_many<T, Q>(&mut self, entities: &mut [Q], paths: Paths) -> Result<u64>
-    where
-        T: Insert,
-        Q: BorrowMut<T>, {
-            insert(&mut self.backend, entities, paths).await
-        }
-
-   
-    pub async fn update_one<T>(&mut self, entity: &mut T, fields: Fields) -> Result<()>
-    where
-        T: Update,
-    {
-        self.update_many::<T, _>(&mut [entity], fields).await
-    }
-
-    pub async fn update_many<T, Q>(&mut self, entities: &mut [Q], fields: Fields) -> Result<()>
-    where
-        T: Update,
-        Q: BorrowMut<T>,
-    {
-            update(&mut self.backend, entities, fields).await
-    }
-
-    /// Load a struct with dependencies for a given Toql query.
-    ///
-    /// Returns a struct or a [ToqlMySqlAsyncError](../toql/error/enum.ToqlMySqlAsyncError.html) if no struct was found _NotFound_ or more than one _NotUnique_.
-    pub async fn load_one<T, B>(&mut self, query: B) -> Result<T>
-    where
-        T: Load<Row, ToqlMySqlAsyncError>,
-        B: Borrow<Query<T>> + Send + Sync,
-        <T as Keyed>::Key: FromRow<Row, ToqlMySqlAsyncError>,
-    {
-        let (mut e, _) = load(&mut self.backend, query, Some(Page::Uncounted(0, 2))).await?;
-        match e.len() {
-            0 => Err(ToqlError::NotFound.into()),
-            1 => Ok(e.pop().unwrap()),
-            _ => Err(ToqlError::NotUnique.into()),
-        }
-    }
-
-    /// Load a vector of structs with dependencies for a given Toql query.
-    ///
-    /// Returns a tuple with the structs and an optional tuple of count values.
-    /// If `count` argument is `false`, no count queries are run and the resulting `Option<(u32,u32)>` will be `None`
-    /// otherwise the count queries are run and it will be `Some((total count, filtered count))`.
-    pub async fn load_many<T, B>(&mut self, query: B) -> Result<Vec<T>>
-    where
-        T: Load<Row, ToqlMySqlAsyncError>,
-        B: Borrow<Query<T>> + Send + Sync,
-        <T as Keyed>::Key: FromRow<Row, ToqlMySqlAsyncError>,
-    {
-      let res = load(&mut self.backend, query, None).await?;
-      Ok(res.0)
-    }
-
-  /// Load a vector of structs with dependencies for a given Toql query.
-    ///
-    /// Returns a tuple with the structs and an optional tuple of count values.
-    /// If `count` argument is `false`, no count queries are run and the resulting `Option<(u32,u32)>` will be `None`
-    /// otherwise the count queries are run and it will be `Some((unpaged count, unfiltered count))`.
-    pub async fn load_page<T, B>(&mut self, query: B, page: Page) -> Result<(Vec<T>, Option<(u64, u64)>)>
-    where
-        T: Load<Row, ToqlMySqlAsyncError>,
-        B: Borrow<Query<T>> + Send + Sync,
-        <T as Keyed>::Key: FromRow<Row, ToqlMySqlAsyncError>,
-    {
-        let entities_page = load(&mut self.backend, query, Some(page)).await?;
-
-        Ok(entities_page)
-    }
-
-    /// Counts the number of rows that match the query predicate.
-    ///
-    /// Returns a struct or a [ToqlMySqlAsyncError](../toql/error/enum.ToqlMySqlAsyncError.html) if no struct was found _NotFound_ or more than one _NotUnique_.
-    pub async fn count<T, B>(&mut self, query: B) -> Result<u64>
-        where
-            T: Count,
-            B: Borrow<Query<T>> + Send + Sync,
-        {
-            count(&mut self.backend, query).await
-        }
-}
+ }
---
+++ src/toql_api.rs
@@ -0,0 +1,133 @@
+
+use async_trait::async_trait;
+
+use toql::keyed::Keyed;
+use toql::page::Page;
+
+use toql::query::Query;
+
+use toql::error::ToqlError;
+use toql::toql_api::ToqlApi;
+ 
+
+use core::borrow::Borrow;
+
+use crate::error::ToqlMySqlAsyncError;
+use crate::MySqlAsync;
+
+use crate::row::Row;
+use toql::prelude::FromRow;
+
+use std::borrow::BorrowMut;
+
+ use toql::toql_api::{fields::Fields, paths::Paths, update::Update, insert::Insert, load::Load, count::Count, delete::Delete};
+ 
+  use toql::backend::{load::load, count::count, insert::insert, update::update, delete::delete};
+  
+
+use crate::mysql_async::prelude::Queryable;
+use mysql_async::Conn;
+
+
+#[async_trait]
+impl<'a, C> ToqlApi  for MySqlAsync<'a, C>
+where C: 'a + Queryable,
+ for<'b> &'b C: std::ops::Deref<Target = Conn>
+ {
+
+    type Row = Row;
+    type Error = ToqlMySqlAsyncError;
+
+  async fn insert_one<T>(&mut self, entity: &mut T, paths: Paths) -> Result<u64, Self::Error>
+    where
+        T: Insert 
+    {
+        self.insert_many::<T, _>(&mut [entity], paths).await
+    }
+
+    /// Insert one struct.
+    ///
+    /// Skip fields in struct that are auto generated with `#[toql(skip_inup)]`.
+    /// Returns the last generated id.
+    async fn insert_many<T, Q>(&mut self, entities: &mut [Q], paths: Paths) -> Result<u64, Self::Error>
+    where
+        T: Insert,
+        Q: BorrowMut<T> + Send, {
+            insert(&mut self.backend, entities, paths).await
+        }
+
+   
+    async fn update_one<T>(&mut self, entity: &mut T, fields: Fields) -> Result<(), Self::Error>
+    where
+        T: Update,
+    {
+        self.update_many::<T, _>(&mut [entity], fields).await
+    }
+
+    async fn update_many<T, Q>(&mut self, entities: &mut [Q], fields: Fields) -> Result<(), Self::Error>
+    where
+        T: Update,
+        Q: BorrowMut<T> + Send,
+    {
+            update(&mut self.backend, entities, fields).await
+    }
+
+    /// Load a struct with dependencies for a given Toql query.
+    ///
+    /// Returns a struct or a [ToqlMySqlAsyncError](../toql/error/enum.ToqlMySqlAsyncError.html) if no struct was found _NotFound_ or more than one _NotUnique_.
+    async fn load_one<T, B>(&mut self, query: B) -> Result<T, Self::Error>
+    where
+        T: Load<Self::Row, Self::Error>,
+        B: Borrow<Query<T>> + Send + Sync,
+        <T as Keyed>::Key: FromRow<Self::Row, Self::Error>,
+    {
+        let (mut e, _) = load(&mut self.backend, query, Some(Page::Uncounted(0, 2))).await?;
+        match e.len() {
+            0 => Err(ToqlError::NotFound.into()),
+            1 => Ok(e.pop().unwrap()),
+            _ => Err(ToqlError::NotUnique.into()),
+        }
+    }
+
+    /// Load a vector of structs with dependencies for a given Toql query.
+    ///
+    /// Returns a tuple with the structs and an optional tuple of count values.
+    /// If `count` argument is `false`, no count queries are run and the resulting `Option<(u32,u32)>` will be `None`
+    /// otherwise the count queries are run and it will be `Some((total count, filtered count))`.
+    async fn load_many<T, B>(&mut self, query: B) -> Result<Vec<T>, Self::Error>
+    where
+        T: Load<Self::Row, Self::Error>,
+        B: Borrow<Query<T>> + Send + Sync,
+        <T as Keyed>::Key: FromRow<Self::Row, Self::Error>,
+    {
+      let res = load(&mut self.backend, query, None).await?;
+      Ok(res.0)
+    }
+
+  /// Load a vector of structs with dependencies for a given Toql query.
+    ///
+    /// Returns a tuple with the structs and an optional tuple of count values.
+    /// If `count` argument is `false`, no count queries are run and the resulting `Option<(u32,u32)>` will be `None`
+    /// otherwise the count queries are run and it will be `Some((unpaged count, unfiltered count))`.
+    async fn load_page<T, B>(&mut self, query: B, page: Page) -> Result<(Vec<T>, Option<(u64, u64)>), Self::Error>
+    where
+        T: Load<Self::Row, Self::Error>,
+        B: Borrow<Query<T>> + Send + Sync,
+        <T as Keyed>::Key: FromRow<Self::Row, Self::Error>,
+    {
+        let entities_page = load(&mut self.backend, query, Some(page)).await?;
+
+        Ok(entities_page)
+    }
+
+    /// Counts the number of rows that match the query predicate.
+    ///
+    /// Returns a struct or a [ToqlMySqlAsyncError](../toql/error/enum.ToqlMySqlAsyncError.html) if no struct was found _NotFound_ or more than one _NotUnique_.
+    async fn count<T, B>(&mut self, query: B) -> Result<u64, Self::Error>
+        where
+            T: Count,
+            B: Borrow<Query<T>> + Send + Sync,
+        {
+            count(&mut self.backend, query).await
+        }
+}
