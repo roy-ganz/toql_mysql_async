d818c36c4da5eca19d62205963df63568dbdc6ca
roy-ganz <roy.ganz@glanis.biz>
2021-07-21 09:51:10 +0200

    Add file

---
+++ src/prelude.rs
@@ -0,0 +1,8 @@
+// Reexport for derive produced code
+pub use mysql_async::prelude::Queryable;
+pub use mysql_async::Conn;
+pub use mysql_async::Value;
+pub use crate::row::Row;
+pub use crate::MySqlAsync;
+pub use crate::error::ToqlMySqlAsyncError;
+
