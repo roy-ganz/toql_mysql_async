72b1daf5b6297d5ef3cf8fd047086f3202b3421c
roy-ganz <roy.ganz@glanis.biz>
2021-10-18 09:36:15 +0200

    impl Display

--- src/error.rs
+++ src/error.rs
@@ -1,6 +1,7 @@
 
 use mysql_async::{Error, FromValueError};
 use toql::{error::ToqlError, sql_builder::sql_builder_error::SqlBuilderError};
+use std::fmt;
 
 #[derive(Debug)]
 pub enum ToqlMySqlAsyncError {
@@ -31,4 +32,16 @@ impl From<SqlBuilderError> for ToqlMySqlAsyncError {
     }
 }
 
+impl fmt::Display for ToqlMySqlAsyncError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            ToqlMySqlAsyncError::ToqlError(e) => { e.fmt(f)}
+            ToqlMySqlAsyncError::MySqlError(e) => {e.fmt(f)}
+            ToqlMySqlAsyncError::FromValueError(e) => {e.fmt(f)}
+        }
+       
+    }
+}
+
+impl std::error::Error for ToqlMySqlAsyncError {}
 
